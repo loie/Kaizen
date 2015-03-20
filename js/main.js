6e39f1e00dfe68059b7ae6a30023b9b01000bc14
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-03-20 17:06:40 +0100

    better size of icons and some beginning of textbox styling

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 /*global window, console, document */
-var preparePage = function () {
+var preparePage,
+    selectArea;
+
+preparePage = function () {
     'use strict';
     var elements,
         i;
     elements = document.getElementsByClassName('icon-big');
     for (i = 0; i < elements.length; i += 1) {
         elements[i].addEventListener('click', function (event) {
-            console.log(event);
+            selectArea(event.toElement);
         });
     }
 };
+
+selectArea = function (element) {
+    console.log(element.addClass("tierchen"));
+};
+
+
 
 window.addEventListener('load', preparePage);
