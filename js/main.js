f48e6b48292c7dd1511d452c3d349944340f8c34
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-03-20 16:38:40 +0100

    better size of icons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-/*global window, console */
-var onLoad = function (event) {
+/*global window, console, document */
+var preparePage = function () {
     'use strict';
-    console.log(event);
+    var elements,
+        i;
+    elements = document.getElementsByClassName('icon-big');
+    for (i = 0; i < elements.length; i += 1) {
+        elements[i].addEventListener('click', function (event) {
+            console.log(event);
+        });
+    }
 };
 
-window.addEventListener('load', onLoad);
+window.addEventListener('load', preparePage);
