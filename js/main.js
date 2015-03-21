b8b3131de22ac3d33d17ecebe8ef4edda8d9e0be
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-03-21 12:49:27 +0100

    go ahead

--- js/main.js
+++ js/main.js
@@ -15,20 +15,29 @@ preparePage = function () {
 };
 
 selectArea = function (element) {
+    'use strict';
+    
+    element.classList.add('class');
+
+document.getElementById("MyElement").classList.remove('class');
+
+if ( document.getElementById("MyElement").classList.contains('class') )
+
+document.getElementById("MyElement").classList.toggle('class');
 
 
-console.log("                ,;~;,");
-console.log("                /\_");
-console.log("               (  /");
-console.log("               (()      //)");
-console.log("               | \\  ,,;;'\ "); 
-console.log("           __ _(  )m=(((((((((((((================--------" );
-console.log("         /'  ' '()/~' '.(, |");
-console.log("      ,;(      )||     |  ~");
-console.log("     ,;' \    /-(.;,   )");
-console.log("          ) /       ) /"); 
-console.log("         //  PjP    ||");
-console.log("        )_\         )_\'");
+    console.log("                ,;~;,");
+    console.log("                /\_");
+    console.log("               (  /");
+    console.log("               (()      //)");
+    console.log("               | \\  ,,;;'\ "); 
+    console.log("           __ _(  )m=(((((((((((((================--------" );
+    console.log("         /'  ' '()/~' '.(, |");
+    console.log("      ,;(      )||     |  ~");
+    console.log("     ,;' \    /-(.;,   )");
+    console.log("          ) /       ) /"); 
+    console.log("         //  PjP    ||");
+    console.log("        )_\         )_\'");
     // console.log(element.addClass("tierchen"));
 };
 
