2ee720f9a6544caf9a10d25bb0f222d8a37a8af4
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-04-01 08:49:36 +0200

    update

--- js/main.js
+++ js/main.js
@@ -3,27 +3,37 @@ var Kaizen = (function (window, document) {
     'use strict';
     var kaizen,
         selectArea,
-        preparePage,
         prepareContent,
         unselectArea;
     kaizen = {
+        init: function () {
+            var elements,
+                i;
+            elements = document.getElementsByClassName('icon-container');
+            for (i = 0; i < elements.length; i += 1) {
+                elements[i].addEventListener('click', selectArea, true);
+            }
+            // prepareContent();
+            // document.getElementById("cancel").addEventListener('click', unselectArea);
+        }
     };
 
     selectArea = function (event) {
-        var element,
-            elements,
-            i;
-        element = event.toElement;
-        element.classList.add('class');
-        elements = document.getElementsByClassName('icon-big');
-        for (i = 0; i < elements.length; i += 1) {
-            if (elements[i] === element) {
-                element.classList.add('active');
-            } else {
-                elements[i].classList.add('inactive');
-            }
-        }
-        document.getElementById('comment').classList.add('active');
+        // var element,
+        //     elements,
+        //     i;
+        // console.log(event);
+        // element = event.toElement;
+        // element.classList.add('class');
+        // elements = document.getElementsByClassName('icon-big');
+        // for (i = 0; i < elements.length; i += 1) {
+        //     if (elements[i] === element) {
+        //         element.classList.add('active');
+        //     } else {
+        //         elements[i].classList.add('inactive');
+        //     }
+        // }
+        // document.getElementById('comment').classList.add('active');
         // document.getElementById("kaizenElement").classList.remove('class');
 
         // if ( document.getElementById("kaizenElement").classList.contains('class') )
@@ -46,54 +56,42 @@ var Kaizen = (function (window, document) {
     };
 
 
-    prepareContent = function () {
-        var json,
-            entries,
-            enjoyed = [],
-            learned = [],
-            improved = [];
+    // prepareContent = function () {
+    //     var json,
+    //         entries,
+    //         enjoyed = [],
+    //         learned = [],
+    //         improved = [];
 
-        json = window.localStorage.getItem('entries');
-        try {
-            entries = JSON.parse(json);
-        } catch (error) {
-            console.err(error);
-        }
-        if (entries !== undefined && entries !== null) {
-            entries.forEach(function (entry) {
-                switch (entry.category) {
-                case 'enjoyed':
-                    enjoyed.push(entry);
-                    break;
-                case 'learned':
-                    learned.push(entry);
-                    break;
-                case 'improved':
-                    improved.push(entry);
-                    break;
-                }
-            });
-        }
-    };
+    //     json = window.localStorage.getItem('entries');
+    //     try {
+    //         entries = JSON.parse(json);
+    //     } catch (error) {
+    //         console.err(error);
+    //     }
+    //     if (entries !== undefined && entries !== null) {
+    //         entries.forEach(function (entry) {
+    //             switch (entry.category) {
+    //             case 'enjoyed':
+    //                 enjoyed.push(entry);
+    //                 break;
+    //             case 'learned':
+    //                 learned.push(entry);
+    //                 break;
+    //             case 'improved':
+    //                 improved.push(entry);
+    //                 break;
+    //             }
+    //         });
+    //     }
+    // };
 
-    unselectArea = function (element) {
-        console.log(element);
-    };
-
-    preparePage = function () {
-        var elements,
-            i;
-        elements = document.getElementsByClassName('icon-big');
-        for (i = 0; i < elements.length; i += 1) {
-            elements[i].addEventListener('click', selectArea);
-        }
-        prepareContent();
-        document.getElementById("cancel").addEventListener('click', unselectArea);
-    };
-
-    window.addEventListener('load', preparePage);
-
+    // unselectArea = function (element) {
+    //     console.log(element);
+    // };
 
     return kaizen;
 }(window, window.document));
 
+
+window.addEventListener('load', Kaizen.init);
