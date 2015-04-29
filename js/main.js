4749b077fa94aaf2b68c1f625f08df87f632b4f0
Lorenz Merdian <lorenz.merdian@googlemail.com>
2015-04-29 07:39:05 +0200

    restored click events

--- js/main.js
+++ js/main.js
@@ -13,32 +13,25 @@ var Kaizen = (function (window, document) {
             for (i = 0; i < elements.length; i += 1) {
                 elements[i].addEventListener('click', selectArea, true);
             }
-            // prepareContent();
+            prepareContent();
             // document.getElementById("cancel").addEventListener('click', unselectArea);
         }
     };
 
     selectArea = function (event) {
-        // var element,
-        //     elements,
-        //     i;
-        // console.log(event);
-        // element = event.toElement;
-        // element.classList.add('class');
-        // elements = document.getElementsByClassName('icon-big');
-        // for (i = 0; i < elements.length; i += 1) {
-        //     if (elements[i] === element) {
-        //         element.classList.add('active');
-        //     } else {
-        //         elements[i].classList.add('inactive');
-        //     }
-        // }
-        // document.getElementById('comment').classList.add('active');
-        // document.getElementById("kaizenElement").classList.remove('class');
-
-        // if ( document.getElementById("kaizenElement").classList.contains('class') )
-        // document.getElementById("kaizenElement").classList.toggle('class');
-
+        var element,
+            elements,
+            i;
+        element = event.toElement;
+        elements = document.getElementsByClassName('icon-big');
+        for (i = 0; i < elements.length; i += 1) {
+            if (elements[i] === element) {
+                element.classList.add('active');
+            } else {
+                elements[i].classList.add('inactive');
+            }
+        }
+        document.getElementById('comment').classList.add('active');
 
         console.log("                ,;~;,");
         console.log("                /\\_");
@@ -56,42 +49,42 @@ var Kaizen = (function (window, document) {
     };
 
 
-    // prepareContent = function () {
-    //     var json,
-    //         entries,
-    //         enjoyed = [],
-    //         learned = [],
-    //         improved = [];
+    prepareContent = function () {
+        var json,
+            entries,
+            enjoyed = [],
+            learned = [],
+            improved = [];
 
-    //     json = window.localStorage.getItem('entries');
-    //     try {
-    //         entries = JSON.parse(json);
-    //     } catch (error) {
-    //         console.err(error);
-    //     }
-    //     if (entries !== undefined && entries !== null) {
-    //         entries.forEach(function (entry) {
-    //             switch (entry.category) {
-    //             case 'enjoyed':
-    //                 enjoyed.push(entry);
-    //                 break;
-    //             case 'learned':
-    //                 learned.push(entry);
-    //                 break;
-    //             case 'improved':
-    //                 improved.push(entry);
-    //                 break;
-    //             }
-    //         });
-    //     }
-    // };
+        json = window.localStorage.getItem('entries');
+        try {
+            entries = JSON.parse(json);
+        } catch (error) {
+            console.err(error);
+        }
+        if (entries !== undefined && entries !== null) {
+            entries.forEach(function (entry) {
+                switch (entry.category) {
+                case 'enjoyed':
+                    enjoyed.push(entry);
+                    break;
+                case 'learned':
+                    learned.push(entry);
+                    break;
+                case 'improved':
+                    improved.push(entry);
+                    break;
+                }
+            });
+        }
+    };
 
-    // unselectArea = function (element) {
-    //     console.log(element);
-    // };
+    unselectArea = function (element) {
+        console.log(element);
+    };
 
     return kaizen;
 }(window, window.document));
 
 
-window.addEventListener('load', Kaizen.inits);
+window.addEventListener('load', Kaizen.init);
